f99ab932becb39a6c99d5c4e2717e7f1ca733b3a
Danny <63234643+impactdot@users.noreply.github.com>
2023-01-12 14:42:20 +0000

    requirements and main changed

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
-from aiogram import Bot, Dispatcher, executor, types
-import requests
-from datetime import datetime
-from dotenv import load_dotenv
-import os
-import asyncio
-
-# import logging
-# logging.basicConfig(level=logging.INFO)
-
-# loading the .env constants
-load_dotenv()
-# Constants from .env
-API_TOKEN = os.environ["API_TOKEN"]
-USER_ID = os.environ["USER_ID"]
-
-# initializing the bot variables required
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
-
-
+from aiogram import executor
+from create_bot import dp
 from handlers import client
 
 client.register_handlers_client(dp)
--- timetest.py
+++
@@ -1,9 +0,0 @@
-import datetime
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-print(datetime.datetime.now().strftime("%s"))
-
-print(os.environ["API_TOKEN"])
