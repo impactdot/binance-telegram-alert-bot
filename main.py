422b2c0100b85952b5f69fa5c2fd69594b579ed4
Danny <63234643+impactdot@users.noreply.github.com>
2023-01-17 22:12:06 +0000

    fastapi with webhooks. async

--- main.py
+++ main.py
@@ -1,28 +1,86 @@
-from flask import Flask, render_template, request
+import asyncio
+import json
+import requests
+from fastapi import FastAPI, Request
+from telegram import Bot, Update
+from decouple import config
 
-import os
-import telegram
-
-app = Flask(__name__)
-
-
-@app.route("/", methods=["GET", "POST"])
-def webhook():
-    bot = telegram.Bot(token=os.environ["YOURAPIKEY"])
-    if request.method == "POST":
-        update = telegram.Update.de_json(request.get_json(force=True), bot)
-        chat_id = update.effective_chat.id
-        text = update.message.text
-        first_name = update.effective_chat.first_name
-        # Reply with the same message
-        bot.sendMessage(chat_id=chat_id, text=f"{text} {first_name}")
-        return "ok"
-    return "error"
+API_TOKEN = config("API_TOKEN")
+app = FastAPI()
+bot = Bot(f"{API_TOKEN}")
 
 
-def index():
-    return webhook()
+async def get_updates(update, symbol):
+    # Get the current price of the cryptocurrency pairing
+    url = "https://api.binance.com/api/v3/ticker/price"
+    params = {"symbol": symbol}
+    response = requests.get(url, params=params)
+    data = json.loads(response.text)
+    current_price = data["price"]
+
+    # Send the current price to the Telegram user
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text=f"The current price of {symbol} is {current_price}",
+    )
 
 
-# https://api.telegram.org/bot5822639543:AAFvPYODp9nmX6_mbYLxr_K7F5FyumLuqcQ/setWebhook?url=https://binance-telegram-alert-bot.herokuapp.com/
-#
+@app.post(f"/{API_TOKEN}")
+async def handle_updates(request: Request):
+    # Extract the update data from the request
+    update = request.json()
+    message = update.get("message")
+    new_symbol = message.get("text")
+
+    global current_task
+    global current_symbol
+
+    # If the user sent a different symbol than the one currently being tracked
+    if current_symbol != new_symbol:
+        # Cancel the current task
+        if current_task:
+            current_task.cancel()
+            current_task = None
+            current_symbol = None
+        current_symbol = new_symbol
+    # Schedule the update task
+
+    async def update_task():
+        while True:
+            try:
+                # Get the current price of the cryptocurrency pairing
+                url = "https://api.binance.com/api/v3/ticker/price"
+                params = {"symbol": current_symbol}
+                response = requests.get(url, params=params)
+                data = json.loads(response.text)
+                current_price = data["price"]
+
+                # Send the current price to the Telegram user
+                bot.send_message(
+                    chat_id=update.message.chat_id,
+                    text=f"The current price of {current_symbol} is {current_price}",
+                )
+                await asyncio.sleep(5)
+            except asyncio.CancelledError:
+                # Handle task cancellation
+                break
+            except Exception as e:
+                # Handle other errors
+                print(f"Error: {e}")
+                break
+
+    if not current_task:
+        current_task = asyncio.create_task(update_task())
+        await current_task
+
+
+async def main():
+    await bot.set_webhook(
+        "https://binance-telegram-alert-bot.herokuapp.com/" + bot.token
+    )
+    await bot.start_webhook(f"/{API_TOKEN}", handle_updates)
+    await app.run()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
